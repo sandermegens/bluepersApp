d4b62fa763397752a1f564a9937fe5367e1339b5
Sander Megens <sandertje86@hotmail.com>
2018-07-07 13:11:37 +0200

    Initial application generated by JHipster-5.0.2

---
+++ src/main/java/nl/bluepers/app/config/ApplicationProperties.java
@@ -0,0 +1,14 @@
+package nl.bluepers.app.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * Properties specific to Bluepers App.
+ * <p>
+ * Properties are configured in the application.yml file.
+ * See {@link io.github.jhipster.config.JHipsterProperties} for a good example.
+ */
+@ConfigurationProperties(prefix = "application", ignoreUnknownFields = false)
+public class ApplicationProperties {
+
+}
